0b4362009233c56fbcefff59d1e8bfaef22872b9
anna <user@users-MacBook-Pro.local>
2024-04-10 17:23:52 +0200

    upload luciddreaming

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,40 @@
+
+// Function to navigate to dynamic route
 function navigateToDynamicRoute(text) {
   var routeName = text.trim().replace(/\s+/g, '-').toLowerCase();
 
-  // Navigate to the dynamically generated route
-  window.location.href = '/' + routeName;
+  // Check if it's not a detailed view page
+  if (!document.body.classList.contains('detailed-view')) {
+      // Navigate to the dynamically generated route
+      window.location.href = '/' + routeName;
+  }
 }
 
+// Function to navigate to dynamic detail route
 function navigateToDynamicDetailRoute(text) {
   var routeName = text.trim().replace(/\s+/g, '-').toLowerCase();
 
-  // Navigate to the dynamically generated route
-  window.location.href = '/detail/' + routeName;
+  // Check if it's not a detailed view page
+  if (!document.body.classList.contains('detailed-view')) {
+      // Navigate to the dynamically generated detail route
+      window.location.href = '/detail/' + routeName;
+  }
+}
+
+// Function to hide image on mouseover
+function hideImage(element) {
+  // Check if it's not a detailed view page
+  if (!document.body.classList.contains('detailed-view')) {
+      // Hide the image
+      element.style.opacity = 0;
+  }
+}
+
+// Function to show image on mouseout
+function showImage(element) {
+  // Check if it's not a detailed view page
+  if (!document.body.classList.contains('detailed-view')) {
+      // Show the image
+      element.style.opacity = 1;
+  }
 }
